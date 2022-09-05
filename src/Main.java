aee66bcf7389b0a4cd01413183efbeeee2ab5932
Nikita <fatyanov_n@mail.ru>
2022-09-05 21:19:28 +0400

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+          task1();
+          task2();
+          task3();
+task4();
+    }
+
+    public static void task1() {
+for (int i = 10; i > -11; i = i - 1) {
+        System.out.println("итерация " + i);
+}
+}
+    public static void task2() {
+        for (int i = 0; i < 11; i = i + 1) {
+            System.out.println("2итерация " + i); }
+    }
+    public static void task3() {
+        for (int i = 0; i < 11; i = i + 1) {
+            System.out.println("3итерация " + i); }
+    }
+    public static void task4() {
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("4итерация " + i); }
+    }
+
+
+}
